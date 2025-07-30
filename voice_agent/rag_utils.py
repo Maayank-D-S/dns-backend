299f1ef0dc4c14dbf710c4ae6b78fb8879e3e10d
Mayank-209 <msknowsevt@gmail.com>
2025-07-30 22:13:48 +0530

    pushing P

--- voice_agent/rag_utils.py
+++ voice_agent/rag_utils.py
@@ -1,18 +1,21 @@
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 import pickle
+import os
 
 def load_faiss_vectorstore():
     index_name = "index"
-    with open(f"{index_name}.pkl", "rb") as f:
+    base_dir = os.path.dirname(os.path.abspath(__file__))  # path to voice_agent/
+    pkl_path = os.path.join(base_dir, f"{index_name}.pkl")
+
+    with open(pkl_path, "rb") as f:
         store = pickle.load(f)
         print("working1")
 
     vectorstore = FAISS.load_local(
-        folder_path=".",  # ← current directory
+        folder_path=base_dir,
         embeddings=OpenAIEmbeddings(),
         index_name=index_name,
-        
         allow_dangerous_deserialization=True
     )
     print("working2")
